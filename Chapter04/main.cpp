52fc66f0094267245197e0f21aca625d2d043003
hosun1997 <hwanghsun@suwon.ac.kr>
2023-03-28 23:31:14 +0900

    Chapter04-02

--- Chapter04/main.cpp
+++ Chapter04/main.cpp
@@ -1,13 +1,20 @@
 #include "1. Camera Input.cpp"
 #include "2. Video Input.cpp"
 #include "3. Camera input Video Output.cpp"
+#include "1. DrawingLine.cpp"
+#include "2. DrawingPolys.cpp"
+#include "3. DrawingText1.cpp"
+#include "4. DrawingText2.cpp"
 
 
 
 int main(void) {
 	//camera_in(); // 内靛 4-2
 	//video_in(); // 内靛 4-3
-	camera_in_video_out(); // 内靛 4-5
-
+	//camera_in_video_out(); // 内靛 4-5
+	//drawLines();	// 内靛 4-6
+	//drawPolys();	// 内靛 4-7
+	//drawText1();	// 内靛 4-8
+	drawText2();	// 内靛 4-9
 	return 0;
 }
